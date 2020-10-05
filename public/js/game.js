371211647053eca80f63dee79922626f15e6e76d
paulohdelia <paulohdelia@gmail.com>
2020-10-04 23:48:32 -0300

    render other players

--- public/js/game.js
+++ public/js/game.js
@@ -21,17 +21,33 @@ const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('ship', 'assets/redSpaceship.png');
+  this.load.image('otherPlayer', 'assets/blackSpaceship.png');
 }
 
 function create() {
   var self = this;
 
   this.socket = io();
+  this.otherPlayers = this.physics.add.group();
 
   this.socket.on('currentPlayers', (players) => {
     Object.keys(players).forEach((id) => {
       if (players[id].playerId === self.socket.id) {
         addPlayer(self, players[id]);
+      } else {
+        addOtherPlayer(self, players[id]);
+      }
+    });
+  });
+
+  this.socket.on('newPlayer', (playerInfo) => {
+    addOtherPlayer(self, playerInfo);
+  });
+
+  this.socket.on('disconnect', (playerId) => {
+    self.otherPlayers.getChildren().forEach((otherPlayer) => {
+      if (playerId === otherPlayer.playerId) {
+        otherPlayer.destroy();
       }
     });
   });
@@ -52,4 +68,18 @@ function addPlayer(self, playerInfo) {
   self.ship.setDrag(100);
   self.ship.setAngularDrag(100);
   self.ship.setMaxVelocity(200);
+}
+
+function addOtherPlayer(self, playerInfo) {
+  const otherPlayer = self.add
+    .sprite(playerInfo.x, playerInfo.y, 'otherPlayer')
+    .setOrigin(0.5, 0.5)
+    .setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') {
+    otherPlayer.setTint(0x0000ff);
+  } else {
+    otherPlayer.setTint(0xff0000);
+  }
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayer);
 }
